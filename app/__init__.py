fc28997ac2ad32a0696579744dbaa0c5a6a8e1e1
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-09 14:43:51 +0300

    create main blueprint in app/main/init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -29,3 +29,11 @@ def create_app(config_name):
     mail.init_app(app)
     bootstrap.init_app(app)
     
+    #register main and auth blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    return app
