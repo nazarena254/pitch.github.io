96a9d904ebe50447d98a46d49dfa69087ebc4ccc
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-09 13:54:11 +0300

    creat class Pitch in models.py file

--- app/models.py
+++ app/models.py
@@ -35,3 +35,23 @@ class User(db.Model,UserMixin):
         
     def __repr__(self):
         return f'User{self.username}'
+
+class Pitch(db.Model):
+    __tablename__ = 'pitches'
+    id = db.Column(db.Integer, primary_key = True)
+    pitch = db.Column(db.Text(),nullable = False)
+    category = db.Column(db.String(255), index = True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    posted = db.Column(db.DateTime,default=datetime.utcnow)
+    like = db.Column(db.Integer,default=1)
+    dislikelike = db.Column(db.Integer)
+    
+    def save_pitch(self):
+        db.session.add(self)
+        db.session.commit()
+        
+    @classmethod
+    def get_pitches(cls,id):
+        pitches = Pitch.query.filter_by(user_id=id).all()
+        return pitches
+
