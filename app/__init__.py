a3634777bd89f1117bb49349c83bbd906cf497d6
nancyngunjiri1 <nancyngunjiri1@gmail.com>
2022-05-09 14:36:50 +0300

    add function that initializes Flask application and other flask extensions

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+# from flask_login._compat import text_type
+from flask_login import LoginManager
+from flask_mail import Mail
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+mail = Mail()
+
+def create_app(config_name): 
+
+    # Initializing application
+    app = Flask(__name__)
+    
+    #Creating the app configurations
+    app.config.from_object(config_options[config_name])
+    
+    
+    # Initializing flask extensions
+    db.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    bootstrap.init_app(app)
+    
